34e8a959d5efe058bda6e40dcd41bc6bc589754c
shetunin48 <shetunin48@yandex.ru>
2023-03-03 00:43:10 +0300

    Подключается к браузеру

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+import socketserver
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+
+class Myserver(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200, "idk1")
+        self.send_header("idk2", "idk3")
+        self.end_headers()
+
+
+server = HTTPServer(('', 8000), Myserver)
+
+print("is running")
+server.serve_forever()
+server.server_close()
+print("server dead")
